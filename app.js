2545c59be83f97c31cfc289e2a8ca53fed4b580a
ogeobubu <ogeobubu@gmail.com>
2024-11-30 13:28:41 +0100

    recent changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require('path');
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
   })
 );
 app.use(express.json());
