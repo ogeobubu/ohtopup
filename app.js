5cc35fbbb1b619d991f86ffc098f047612dab11a
ogeobubu <ogeobubu@gmail.com>
2024-12-01 05:20:26 +0100

    recent changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const walletRoutes = require("./routes/walletRoutes");
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(
@@ -14,12 +14,6 @@ app.use(
   })
 );
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,6 +25,12 @@ mongoose
 app.use("/api/users", userRoutes);
 app.use("/api/users/admin", adminRoutes);
 app.use("/api/users/wallet", walletRoutes);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
