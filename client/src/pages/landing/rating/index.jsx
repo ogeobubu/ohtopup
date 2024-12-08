5a4564eee4ab6ab22f8adf950482a63d86187cbe
ogeobubu <ogeobubu@gmail.com>
2024-12-09 00:54:25 +0100

    recent changes

--- client/src/pages/landing/rating/index.jsx
+++ client/src/pages/landing/rating/index.jsx
@@ -1,6 +1,6 @@
 const Rating = () => (
     <section className="py-20 bg-white">
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center p-2">
         <h3 className="text-3xl font-bold mb-6">Customer Ratings</h3>
         <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-6">
           <div className="text-yellow-500 text-4xl sm:text-5xl">⭐⭐⭐⭐⭐</div>
